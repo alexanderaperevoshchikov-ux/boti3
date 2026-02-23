117544d83e6497b8e982c3afa6fa7104e929643c
Alexander <alexander.a.perevoshchikov@gmail.com>
2026-02-23 19:52:12 +0300

    База данных

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import telebot
 from pyexpat.errors import messages
+import sqlite3
 
 token = "8694316841:AAFag6C4pXcq4A5P7nKeJTzjBcB0dgC3vj8"
 bot = telebot.TeleBot(token)
+conn = sqlite3.connect('base.db')
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS user_appointment
+                (user_id INTEGER, 
+                 date TEXT, 
+                 client TEXT,
+                 PRIMARY KEY (user_id, date))''')
 
-
+def add_appointment(date,time,client):
+    pass
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "Дратуте")
